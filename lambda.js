143beeffb5547615b2bf8fdf366e7bf431ec9f52
Rohan Patil <131379496+Rohan9335@users.noreply.github.com>
2023-05-08 14:53:43 +0530

    Add files via upload

---
+++ lambda.js
@@ -0,0 +1,14 @@
+
+            //LAMBDA / FAT ARROW NOTATION
+
+            // function f1(a,b){
+            //     return a+b
+            // }
+            // console.log(f1(1,2))
+            
+             let s= (a,b)=> (a+b)*2
+            // let x=s(1,2)
+            // console.log(x)
+            console.log(s(1,9))
+            
+            
