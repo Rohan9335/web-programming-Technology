30d37327fcd0d24a7c9c48adfefa4ba1fb366506
Rohan Patil <131379496+Rohan9335@users.noreply.github.com>
2023-05-08 14:53:10 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,8 @@
+
+
+            let arr=[1,2,3,4,5,6,7,8]
+
+            console.log(arr.filter((x)=>x%2!=0))
+            let arr2=["red","green","blue","white","black_and_white"]
+
+            console.log(arr2.filter((x)=>x.length<6))
