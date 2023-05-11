deb26f90232f1e29c6d1f330d858f77471c5ccdf
Rohan Patil <131379496+Rohan9335@users.noreply.github.com>
2023-05-11 20:05:44 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,32 @@
+
+console.log("1")
+setTimeout  (() => { console.log("hello")},3000);
+console.log("2")
+setInterval  (() => { console.log("hi")},3000);
+console.log("2")
+// class Product {
+
+//     constructor(name, cost, desc) {
+//         this.pName = name;
+//         this.pCost = cost;
+//         this.pDesc = desc;
+//     }
+//     showProductdetails(arr) {
+
+//         for (let x of arr) {
+//             console.log(x)
+//        }
+//     }
+// }
+// let obj = new Product("eraser", 5, "camlin")
+// let products = []
+
+// products.push(new Product("pen", 10, "blue"))
+// products.push(new Product("pencil", 20, "Camlin"))
+// products.push(new Product("Notebook", 70, "Classmate"))
+// //obj.showProductdetails(products);
+
+
+// obj2 = ({ name:"samarth", age : 60 })
+
+// console.log(obj2)
