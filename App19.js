4ff8e0bc10d3cc3013a4a2cd070fd0242df8a790
Rohan Patil <131379496+Rohan9335@users.noreply.github.com>
2023-05-19 19:58:30 +0530

    Add files via upload

---
+++ App19.js
@@ -0,0 +1,15 @@
+import Ajaxwithreact from "./Ajaxwithreact";
+
+
+    export default function App19()
+    {
+
+
+
+        return <div>
+
+
+             <Ajaxwithreact></Ajaxwithreact>   
+
+        </div>
+    }
